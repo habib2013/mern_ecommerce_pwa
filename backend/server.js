874e8673e5aa1c244fed4598998c2c03f1804c6c
habib2013 <nairabet25@gmail.com>
2020-08-02 14:00:09 +0100

    --- Auto-Git Commit ---

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,30 @@
-import express from 'express';
-import config from './config';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+const express =  require('express');
+const cors = require('cors');
 import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
 import bodyParser from 'body-parser';
-// New changes
-import cors from 'cors';
+const mongoose = require('mongoose'); 
 
-dotenv.config();
-const mongodbUrl = config.MONGODB_URL;
+require('dotenv').config();
+
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5003;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGODB_URL;
-mongoose.connect(uri ,{useUrlParser:true,useCreateIndex:true}
+mongoose.connect(uri ,{useCreateIndex:true}
    );
    const connection = mongoose.connection;
    connection.once('open', () => {
       console.log("MongoDB database connection established successfully");   
    })
-
    app.use(bodyParser.json());
    app.use('/api/users', userRoute);
    app.use('/api/products/',productRoute);
+
 
 app.listen(port,()=> {
    console.log(`Server is running on port: ${port}`);
