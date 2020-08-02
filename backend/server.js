d78580966e2e4530fbc2502ad6502f9814c312bd
habib2013 <nairabet25@gmail.com>
2020-08-02 14:20:15 +0100

    --- Auto-Git Commit ---

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,26 @@
-const express =  require('express');
-const cors = require('cors');
+import express from 'express';
+import config from './config';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+import productRoute from './routes/productRoute';
+import bodyParser from 'body-parser';
+// New changes
+import cors from 'cors';
 
-const mongoose = require('mongoose'); 
 
-require('dotenv').config();
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl,{
+   useNewUrlParser:true,
+   useUnifiedTopology: true,
+   useCreateIndex:true
+}).catch(error => console.log(error.reason));
 
 const app = express();
-const port = process.env.PORT || 5002;
-
-app.use(cors());
-app.use(express.json());
-
-const uri = process.env.MONGODB_URL;
-mongoose.connect(uri ,{useUrlParser:true,useCreateIndex:true}
-   );
-   const connection = mongoose.connection;
-   connection.once('open', () => {
-      console.log("MongoDB database connection established successfully");   
-   })
+app.use(bodyParser.json());
+app.use('/api/users', userRoute);
+app.use('/api/products/',productRoute);
 
 
-  
-
-
-app.listen(port,()=> {
-   console.log(`Server is running on port: ${port}`);
-});
+app.listen(5002,() => {console.log('Server running on localhost http://localhost:5002')})
