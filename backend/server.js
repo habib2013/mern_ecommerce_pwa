be05305018fa26544734218f48090667473e5d79
habib2013 <nairabet25@gmail.com>
2020-08-02 13:35:24 +0100

    --- Auto-Git Commit ---

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
 import bodyParser from 'body-parser';
+// New changes
+import cors from 'cors';
 
 
 dotenv.config();
@@ -20,19 +22,5 @@ app.use(bodyParser.json());
 app.use('/api/users', userRoute);
 app.use('/api/products/',productRoute);
 
-// app.get('/api/products/:id',(req,res) => {
-//    const productId = req.params.id;
-//   const product = (data.products.find(x => x._id == productId));
-//    if(product){
-//       res.send(product);
-//    }
-//    else{
-//       res.status(404).send({msg : "Product Not Found"});
-//    }
-// });
-
-// app.get('/api/products',(req,res) => {
-//    res.send(data.products);
-// });
 
 app.listen(5001,() => {console.log('Server running on localhost http://localhost:5001')})
