8608e18340cf3d7f4054d921f0ccb00540351449
habib2013 <nairabet25@gmail.com>
2020-08-02 14:10:13 +0100

    --- Auto-Git Commit ---

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export default {
-   MONGODB_URL : process.env.MONGODB_URL,
+   MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost/excwha',
    JWT_SECRET : process.env.JWT_SECRET || 'somethingsecret'
 }
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,26 @@
 const express =  require('express');
 const cors = require('cors');
-import userRoute from './routes/userRoute';
-import productRoute from './routes/productRoute';
-import bodyParser from 'body-parser';
+
 const mongoose = require('mongoose'); 
 
 require('dotenv').config();
 
-
 const app = express();
-const port = process.env.PORT || 5003;
+const port = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGODB_URL;
-mongoose.connect(uri ,{useCreateIndex:true}
+mongoose.connect(uri ,{useUrlParser:true,useCreateIndex:true}
    );
    const connection = mongoose.connection;
    connection.once('open', () => {
       console.log("MongoDB database connection established successfully");   
    })
-   app.use(bodyParser.json());
-   app.use('/api/users', userRoute);
-   app.use('/api/products/',productRoute);
+
+
+  
 
 
 app.listen(port,()=> {
