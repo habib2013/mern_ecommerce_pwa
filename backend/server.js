bf26eeb4abbc74fd0a5a7ff84426fdc7b684fe98
habib2013 <nairabet25@gmail.com>
2020-08-02 13:45:10 +0100

    --- Auto-Git Commit ---

--- backend/server.js
+++ backend/server.js
@@ -8,19 +8,27 @@ import bodyParser from 'body-parser';
 // New changes
 import cors from 'cors';
 
-
 dotenv.config();
 const mongodbUrl = config.MONGODB_URL;
-mongoose.connect(mongodbUrl,{
-   useNewUrlParser:true,
-   useUnifiedTopology: true,
-   useCreateIndex:true
-}).catch(error => console.log(error.reason));
 
 const app = express();
-app.use(bodyParser.json());
-app.use('/api/users', userRoute);
-app.use('/api/products/',productRoute);
+const port = process.env.PORT || 5001;
 
+app.use(cors());
+app.use(express.json());
 
-app.listen(5001,() => {console.log('Server running on localhost http://localhost:5001')})
+const uri = process.env.MONGODB_URL;
+mongoose.connect(uri ,{useUrlParser:true,useCreateIndex:true}
+   );
+   const connection = mongoose.connection;
+   connection.once('open', () => {
+      console.log("MongoDB database connection established successfully");   
+   })
+
+   app.use(bodyParser.json());
+   app.use('/api/users', userRoute);
+   app.use('/api/products/',productRoute);
+
+app.listen(port,()=> {
+   console.log(`Server is running on port: ${port}`);
+});
